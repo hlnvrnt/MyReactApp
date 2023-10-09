46058e8329e02289ca4c4ab9333c4bad2f01bddd
hlnvrnt <helene.vernet@live.fr>
2023-10-09 17:05:59 +0200

    pokemon

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import PokemonCard from './components/pokemonCard.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
+      <div><PokemonCard /></div>
+  )
+}
+
+export default App
+
+// const [count, setCount] = useState(0)
+/* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -27,9 +32,4 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+      </p> */
