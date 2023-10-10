73e7dd9d44eb611478b05da3c9281f03bcfd7f72
hlnvrnt <helene.vernet@live.fr>
2023-10-10 10:20:47 +0200

    pokemon 2

--- src/components/pokemonCard.jsx
+++ src/components/pokemonCard.jsx
@@ -1,8 +1,31 @@
 function PokemonCard (){
-    return <figure>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" />
+    const pokemon = pokemonList[0];
+    if (Object.hasOwn(pokemon, "imgSrc")){
+        return  <figure>
+        <img src = {pokemon.imgSrc} />
         <figcaption>Bulbasaur</figcaption>
-    </figure>
+        </figure>
+        
+    }else {
+       return <figure>
+       <p>????</p>
+       <figcaption>{pokemon.name}</figcaption>
+       </figure>
+    }
 }
 
+// for (let i=0; i<pokemonList.length; i++){
+    // if (pokemonList[i].imgSrc !== ""){
+
+
 export default PokemonCard
+
+const pokemonList = [
+    {
+        name : "bulbasaur",
+        imgSrc : "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+        name : "mew",
+    },
+];
